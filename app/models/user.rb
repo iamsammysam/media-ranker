634cb290e825e648266ac178dd272bd77417a8e4
Samantha Coll <samcoll@Samanthas-MBP.localdomain>
2019-10-17 17:56:29 -0700

    made fixtures and added user validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  has_many :votes #, dependent: :nullify
-  validates :name, presence: true
+  has_many :votes, dependent: :nullify
+  
+  validates :name, presence: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
-end
+  describe "validations" do 
+    before do
+      @user = User.new(name: "test user")
+    end
+    
+    it "is valid when all fields are present" do
+      result = @user.valid?
+      expect(result).must_equal true
+    end
+    
+    it "is invalid without a name" do 
+      @user.name = nil
+      result = @user.valid?
+      expect(result).must_equal false
+    end 
+    
+    it "is invalid if the name is not unique" do
+      @user.name = users(:ada).name
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :name
+    end
+  end 
+end 
