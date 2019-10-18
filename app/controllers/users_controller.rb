1c3d8cb342029fcc275d89834e5f7e6089011ffe
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-10-17 19:02:07 -0700

    added users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all  
+  end
+  
+  def show
+    user_id = params[:id]
+    @user = User.find_by(id: user_id)
+    
+    if @user.nil?
+      # redirect_to works_path
+      return 
+    end   
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(vote_params) 
+    if @user.save 
+      # redirect_to work_path(@work.id)
+      return
+    else
+      render :new
+      return
+    end
+  end
+  
+  private
+  
+  def user_params
+    return params.require(:user).permit(:name)
+  end
 end
