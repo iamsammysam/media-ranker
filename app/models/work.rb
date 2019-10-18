8f5022b5f4b1c2259f31d7bc3804b892f8a71815
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-10-17 18:51:01 -0700

    made fixtures and added works validation tests

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,11 +1,12 @@
 class Work < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :nullify
   
   validates :category, presence: true
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :creator, presence: true
   validates :publication_year, presence: true
   validates :description, presence: true
+  
   
   def self.sort_by_category(category)
     self.where(category: category)
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do 
+    before do
+      @work = Work.new(category: "music", title: "smaple title", creator: "sample creator", description: "great music", publication_year: 2000)
+    end
+    
+    it "is valid when all fields are present" do
+      result = @work.valid?
+      expect(result).must_equal true
+    end
+    
+    it "is invalid without a title" do 
+      @work.title = nil
+      result = @work.valid?
+      expect(result).must_equal false
+    end 
+    
+    it "is valid without a creator" do
+      @work.creator = nil
+      result = @work.valid?
+      expect(result).must_equal false
+    end
+    
+    it "is valid without a description" do
+      @work.description = nil
+      result = @work.valid?
+      expect(result).must_equal false
+    end
+    
+    it "is valid wihout a publication year" do
+      @work.publication_year = nil
+      result = @work.valid?
+      expect(result).must_equal false
+    end
+    
+    it "is invalid if the title is not unique" do
+      @work.title = works(:toy).title
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title
+    end
+  end 
 end
